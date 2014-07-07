9bbb5ff19653f425cf73406164204d41dc9cf66c
John Foster <john.foster@utsa.edu>
2014-07-06 23:30:08 -0500

    First commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM johntfoster/netcdf:largefiles
+
+MAINTAINER John Foster <johntfosterjr@gmail.com>
+
+ENV HOME /root
+
+RUN apt-get -yq install gfortran python
+RUN apt-get -yq install libblas-dev liblapack-dev
+RUN apt-get -yq install libboost-dev
+RUN apt-get -yq install libopenmpi-dev
+RUN apt-get -yq install cmake
+
+#Build Trilinos
+RUN wget http://trilinos.sandia.gov/download/files/trilinos-11.8.1-Source.tar.bz2;\
+    tar xjvf trilinos-11.8.1-Source.tar.bz2;\
+    mkdir trilinos-11.8.1-Source/build
+ADD trilinos-debian-cmake.sh /trilinos-11.8.1-Source/build/trilinos-debian-cmake.sh
+RUN cd trilinos-11.8.1-Source/build;\
+    ./trilinos-debian-cmake.sh;\
+    make -j8 && make install;\
+    cd ../..;\
+    rm -rf hdf5-1.8.13 hdf5-1.8.13.tar.bz2 
+
---
+++ trilinos-debian-cmake.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+rm -f CMakeCache.txt 
+
+MPI_HOME=/usr
+EXTRA_ARGS=$@
+
+cmake -D CMAKE_INSTALL_PREFIX:PATH=/usr/local/trilinos/ \
+  \
+  -D MPI_BASE_DIR:PATH=/usr \
+  \
+  -D CMAKE_BUILD_TYPE:STRING=Release \
+  -D CMAKE_Fortran_COMPILER:FILEPATH=/usr/bin/mpif90 \
+  -D BUILD_SHARED_LIBS:BOOL=OFF \
+  -D Trilinos_WARNINGS_AS_ERRORS_FLAGS:STRING="" \
+  \
+  -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
+  -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
+  -D Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES:BOOL=ON \
+  -D Trilinos_ENABLE_Teuchos:BOOL=ON \
+  -D Trilinos_ENABLE_Shards:BOOL=ON \
+  -D Trilinos_ENABLE_Sacado:BOOL=ON \
+  -D Trilinos_ENABLE_Epetra:BOOL=ON \
+  -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
+  -D Trilinos_ENABLE_Ifpack:BOOL=ON \
+  -D Trilinos_ENABLE_AztecOO:BOOL=ON \
+  -D Trilinos_ENABLE_Belos:BOOL=ON \
+  -D Trilinos_ENABLE_Phalanx:BOOL=ON \
+  -D Phalanx_EXPLICIT_TEMPLATE_INSTANTIATION:BOOL=ON \
+  -D Trilinos_ENABLE_Zoltan:BOOL=ON \
+  -D Trilinos_ENABLE_SEACAS:BOOL=ON \
+  -D Trilinos_ENABLE_SECONDARY_STABLE_CODE:BOOL=ON \
+  -D Trilinos_ENABLE_NOX:BOOL=ON \
+  -D Trilinos_ENABLE_Intrepid:BOOL=ON \
+  -D Trilinos_ENABLE_Isorropia:BOOL=ON \
+  -D Trilinos_ENABLE_SEACASPLT=OFF \
+  -D Trilinos_ENABLE_SEACASBlot=OFF \
+  -D Trilinos_ENABLE_SEACASFastq=OFF \
+  \
+  -D Trilinos_ENABLE_EXAMPLES:BOOL=OFF \
+  -D Trilinos_ENABLE_TESTS:BOOL=OFF \
+  \
+  -D TPL_ENABLE_Netcdf:BOOL=ON \
+  -D TPL_ENABLE_MATLAB:BOOL=OFF \
+  -D TPL_ENABLE_Matio:BOOL=OFF \
+  -D Netcdf_INCLUDE_DIRS:PATH=/usr/local/netcdf/include \
+  -D Netcdf_LIBRARY_DIRS:PATH=/usr/local/netcdf/lib \
+  -D TPL_ENABLE_MPI:BOOL=ON \
+  -D TPL_ENABLE_BLAS:BOOL=ON \
+  -D TPL_ENABLE_LAPACK:BOOL=ON \
+  -D TPL_LAPACK_LIBRARIES:STRING=/usr/lib/liblapack.dylib \
+  -D TPL_ENABLE_Boost:BOOL=ON \
+  -D TPL_Boost_INCLUDE_DIRS:PATH=/usr/include/boost \
+  -D TPL_ENABLE_QT:BOOL=OFF \
+  -D TPL_ENABLE_X11:BOOL=OFF \
+  \
+  -D CMAKE_VERBOSE_MAKEFILE:BOOL=OFF \
+  -D Trilinos_VERBOSE_CONFIGURE:BOOL=OFF \
+  \
+  $EXTRA_ARGS \
+  ..
